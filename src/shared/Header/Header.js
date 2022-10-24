51f0e63d465eaafc3f42e75b3bd039aa3e6284fb
farukphero <farukk6319@gmail.com>
2022-10-25 01:42:22 +0600

    NavBar responsive

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -4,12 +4,12 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="absolute  md:top-28 top-64 left-0 lg:left-20 whitespace-pre-wrap pl-3">
-        <h1>WELCOME TO EDUCATION BUDDIES</h1>
-      <h1 className="text-7xl md:text-5xl lg:text-5xl md:w-2/3">
-        Best Online Education Expertise
+    <div className="absolute top-64 md:top-28 lg:top-64 left-0 lg:left-20 whitespace-pre-wrap pl-3">
+        <h1> WELCOME TO EDUCATION BUDDIES</h1>
+      <h1 className="text-6xl md:text-5xl lg:text-5xl md:w-2/3">
+        <b>Best Online Education Expertise</b>
     </h1>
-    <button className="btn btn-active btn-ghost mt-10">View Courses   <ArrowSmallRightIcon className="h-6 w-6"/></button>
+    <button className="btn btn-active btn-info md:btn-ghost lg:btn-ghost mt-10">View Courses<ArrowSmallRightIcon className="h-6 w-6"/></button>
     </div>
   );
 };
